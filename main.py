05936f2e866d89ab0f0233a8ed753e638eef6ffb
Caio Assis <caioassis@Users-MacBook-Pro.local>
2024-06-03 14:11:13 -0300

    refact(rota_post):adicionando a rota de post para update dos valores.

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request 
 from bd_professores import Professores
 
 app = Flask(__name__)
-    
+app.config['JSON_SORT_KEYS'] = False
     
 @app.route('/professores', methods=['GET'])
 def get_professores():
     return make_response(
-        jsonify (
+        jsonify(
             Professores
+            )
         )
-    )
 
-
+@app.route('/professores', methods=['POST'])
+def create_professor():
+    professor = request.json
+    Professores.append(professor)
+    return Professores
+    
 app.run()
 
