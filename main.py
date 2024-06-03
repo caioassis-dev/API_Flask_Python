92b24da5c617b130500560ddd1a4617d2958c17d
Caio Assis <caioassis@Users-MacBook-Pro.local>
2024-06-03 12:04:54 -0300

    refact(app_route):adicionando a rota de GET para retornat Json dos dados dos professores.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
+from bd_professores import Professores
 
 app = Flask(__name__)
     
+    
+@app.route('/professores', methods=['GET'])
+def get_professores():
+    return make_response(
+        jsonify (
+            Professores
+        )
+    )
+
 
 app.run()
 
