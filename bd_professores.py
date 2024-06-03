fa93cf227b827a91fbd5399c3441ed723d93abfa
Caio Assis <caioassis@Users-MacBook-Pro.local>
2024-06-03 12:03:46 -0300

    fix(dados_professores):adicionando novos campos na lista de professores.

--- bd_professores.py
+++ bd_professores.py
@@ -1,32 +1,44 @@
-professores = [
+Professores = [
     {
         'id':1,
         'disciplina':'Matemática',
         'cargaHoraria': 60,
-        'nome':'Fabiano'
+        'nome':'Fabiano',
+        'salario':3600
     },
     {
         'id':2,
         'disciplina':'Geografia',
         'cargaHoraria': 60,
-        'nome':'Tatiana'
+        'nome':'Tatiana',
+        'salario':3600
     },
     {
         'id':3,
         'disciplina':'Física',
         'cargaHoraria': 60,
-        'nome':'Nilton'
+        'nome':'Nilton',
+        'salario':3600
     },
     {
         'id':4,
         'disciplina':'História',
         'cargaHoraria': 60,
-        'nome':'Janaina'
+        'nome':'Janaina',
+        'salario':3600
     },
     {
         'id':5,
         'disciplina':'Inglês',
         'cargaHoraria': 40,
-        'nome':'Gorete'
+        'nome':'Gorete',
+        'salario':2900
+    },
+    {
+        'id':6,
+        'disciplina':'Ed.Física',
+        'cargaHoraria': 20,
+        'nome':'Gorete',
+        'salario':1800
     },
 ]
