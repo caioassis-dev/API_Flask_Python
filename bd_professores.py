767a1818aa3d68d7a9e7487cf41525893be7a0df
Caio Assis <caioassis@Users-MacBook-Pro.local>
2024-06-03 11:41:10 -0300

    feat(dataBase_provisorio):adicionando database provisorio, apenas de teste inicial.

---
+++ bd_professores.py
@@ -0,0 +1,32 @@
+professores = [
+    {
+        'id':1,
+        'disciplina':'Matemática',
+        'cargaHoraria': 60,
+        'nome':'Fabiano'
+    },
+    {
+        'id':2,
+        'disciplina':'Geografia',
+        'cargaHoraria': 60,
+        'nome':'Tatiana'
+    },
+    {
+        'id':3,
+        'disciplina':'Física',
+        'cargaHoraria': 60,
+        'nome':'Nilton'
+    },
+    {
+        'id':4,
+        'disciplina':'História',
+        'cargaHoraria': 60,
+        'nome':'Janaina'
+    },
+    {
+        'id':5,
+        'disciplina':'Inglês',
+        'cargaHoraria': 40,
+        'nome':'Gorete'
+    },
+]
